ee2e4f894cd684d3e1542ca51c7ed611e4a44cac
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-26 09:40:03 +0300

    initial commit

---
+++ app/src/main/java/ercanduman/visualizerestaurant/MainActivity.kt
@@ -0,0 +1,11 @@
+package ercanduman.visualizerestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
