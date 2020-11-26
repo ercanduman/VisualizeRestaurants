e1ba6f828f055bac774f87bdf81f3bff7ed86ae0
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-26 09:53:16 +0300

    Dependencies added

--- app/src/main/java/ercanduman/visualizerestaurant/ui/MainActivity.kt
+++ app/src/main/java/ercanduman/visualizerestaurant/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ercanduman.visualizerestaurant
+package ercanduman.visualizerestaurant.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ercanduman.visualizerestaurant.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
