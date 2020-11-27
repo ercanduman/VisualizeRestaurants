e79ca5922403e6c14468eb88002799725f7084a1
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-27 10:54:53 +0300

    AppAdapter.kt implementation added to MainActivity.kt

--- app/src/main/java/ercanduman/visualizerestaurant/ui/main/MainActivity.kt
+++ app/src/main/java/ercanduman/visualizerestaurant/ui/main/MainActivity.kt
@@ -6,22 +6,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import ercanduman.visualizerestaurant.R
-import ercanduman.visualizerestaurant.ui.utils.logd
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: AppViewModel by viewModels()
+    private lateinit var appAdapter: AppAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launch {
-            viewModel.getRestaurants().observe(this@MainActivity, {
-                logd("Retrieved restaurants from local data source size: ${it.size}")
-                logd(it.toString())
-            })
-        }
+
+        appAdapter = AppAdapter()
+        setupRecyclerView()
+
+        loadData()
+    }
+
+    private fun loadData() = lifecycleScope.launch {
+        viewModel.getRestaurants().observe(this@MainActivity, {
+            appAdapter.submitList(it)
+        })
+    }
+
+    private fun setupRecyclerView() = main_recycler_view_restaurants.apply {
+        adapter = appAdapter
     }
 }
