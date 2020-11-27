6be86e5fa4884df65b2e38b2a089ca5df7542913
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-28 00:19:39 +0300

    Filtering functionality finished

--- app/src/main/java/ercanduman/visualizerestaurant/ui/main/MainActivity.kt
+++ app/src/main/java/ercanduman/visualizerestaurant/ui/main/MainActivity.kt
@@ -5,21 +5,26 @@ import android.view.View
 import android.widget.AdapterView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import ercanduman.visualizerestaurant.R
+import ercanduman.visualizerestaurant.data.db.entity.Restaurant
 import ercanduman.visualizerestaurant.ui.utils.hide
 import ercanduman.visualizerestaurant.ui.utils.show
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_toolbar.*
 import kotlinx.android.synthetic.main.activity_main_toolbar_search.*
 import kotlinx.coroutines.launch
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), AppAdapter.ItemClickListener {
 
     private val viewModel: AppViewModel by viewModels()
     private lateinit var appAdapter: AppAdapter
+
+    private lateinit var mRestaurant: List<Restaurant>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +41,51 @@ class MainActivity : AppCompatActivity(), AppAdapter.ItemClickListener {
     private fun initToolbarActions() {
         main_toolbar_search_icon.setOnClickListener {
             main_toolbar.hide()
-            main_parent_toolbar_search.show()
+            main_toolbar_search.show()
         }
-        search_toolbar_cancel.setOnClickListener {
+        main_toolbar_search_cancel.setOnClickListener {
             main_toolbar.show()
-            main_parent_toolbar_search.hide()
+            main_toolbar_search.hide()
+        }
+        applySearch()
+    }
+
+    private fun applySearch() {
+        main_toolbar_search_text_field.apply {
+            addTextChangedListener {
+                val searchText = text.toString().toLowerCase(Locale.getDefault())
+
+                val filteredList = mutableListOf<Restaurant>()
+                if (searchText.isEmpty()) {
+                    filteredList.addAll(mRestaurant)
+                } else {
+                    filteredList.addAll(
+                        mRestaurant.filter {
+                            it.name.toLowerCase(Locale.getDefault()).contains(searchText)
+                        })
+                }
+
+                if (filteredList.isEmpty()) {
+                    val message: String = getString(R.string.format_item_not_found, searchText)
+                    showContent(false, message)
+                } else {
+                    showContent(true, null)
+                    appAdapter.submitList(filteredList)
+                }
+            }
+        }
+    }
+
+    private fun showContent(show: Boolean, message: String?) {
+        if (show) {
+            main_recycler_view_restaurants.show()
+            main_not_found.hide()
+        } else {
+            main_recycler_view_restaurants.hide()
+            main_not_found.apply {
+                show()
+                text = message
+            }
         }
     }
 
@@ -60,7 +105,8 @@ class MainActivity : AppCompatActivity(), AppAdapter.ItemClickListener {
 
     private fun loadData() = lifecycleScope.launch {
         viewModel.getRestaurants().observe(this@MainActivity, {
-            appAdapter.submitList(it)
+            mRestaurant = it
+            appAdapter.submitList(mRestaurant)
         })
     }
 
