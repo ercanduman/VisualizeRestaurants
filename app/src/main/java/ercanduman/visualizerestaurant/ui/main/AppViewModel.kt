a0892a8d04dd3784c1efabebb4cf052eb5c475be
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-27 17:00:31 +0300

    Sorting functionality added

--- app/src/main/java/ercanduman/visualizerestaurant/ui/main/AppViewModel.kt
+++ app/src/main/java/ercanduman/visualizerestaurant/ui/main/AppViewModel.kt
@@ -1,6 +1,8 @@
 package ercanduman.visualizerestaurant.ui.main
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ercanduman.visualizerestaurant.data.db.entity.Restaurant
@@ -20,17 +22,56 @@ class AppViewModel
     var sortType = SortType.popularity
 
     /**
+     * Updates object and send parameter object to repository.
+     * @param restaurant Restaurant
+     */
+    internal fun update(restaurant: Restaurant) {
+        viewModelScope.launch { repository.update(restaurant) }
+    }
+
+    private val allItems = MediatorLiveData<List<Restaurant>>()
+
+    /**
      * Fetches all data from repository and returns LiveData of restaurant list.
      *
      * @return LiveData<List<Restaurant>>
      */
-    suspend fun getRestaurants() = repository.getRestaurants()
-
-    /**
-     * Updates object and send parameter object to repository.
-     * @param restaurant Restaurant
-     */
-    fun update(restaurant: Restaurant) {
-        viewModelScope.launch { repository.update(restaurant) }
+    internal fun getRestaurants(): LiveData<List<Restaurant>> {
+        viewModelScope.launch {
+            allItems.addSource(repository.getRestaurants()) { items ->
+                allItems.value = sortItems(items)
+            }
+        }
+        return allItems
     }
+
+    private fun sortItems(items: List<Restaurant>) =
+        items.sortedWith(
+            compareByDescending<Restaurant> { it.isFavorite }   // favorites always at top.
+                .thenComparing(::compareByOpeningState)         // Sort based on Opening state
+                .thenComparing { o1, o2 ->
+                    compareByBestMatch(o1, o2, sortType.name) // Sort based on Sorting Option
+                }
+        )
+
+    private fun compareByBestMatch(
+        o1: Restaurant,
+        o2: Restaurant,
+        fieldName: String
+    ): Int {
+        return when (fieldName) {
+            "averageProductPrice" -> o1.sortingValues.averageProductPrice.compareTo(o2.sortingValues.averageProductPrice)
+            "bestMatch" -> o1.sortingValues.bestMatch.compareTo(o2.sortingValues.bestMatch)
+            "deliveryCosts" -> o1.sortingValues.deliveryCosts.compareTo(o2.sortingValues.deliveryCosts)
+            "distance" -> o1.sortingValues.distance.compareTo(o2.sortingValues.distance)
+            "minCost" -> o1.sortingValues.minCost.compareTo(o2.sortingValues.minCost)
+            "newest" -> o1.sortingValues.newest.compareTo(o2.sortingValues.newest)
+            "popularity" -> o1.sortingValues.popularity.compareTo(o2.sortingValues.popularity)
+            "ratingAverage" -> o1.sortingValues.ratingAverage.compareTo(o2.sortingValues.ratingAverage)
+            else -> 0
+        }
+    }
+
+    private fun compareByOpeningState(o1: Restaurant, o2: Restaurant) =
+        o1.status.length.compareTo(o2.status.length)
 }
