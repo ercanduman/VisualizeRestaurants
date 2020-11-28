b3b671049895d836d748deb1d89dcd716407a983
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-11-28 18:53:30 +0300

    UITestSuite.kt created in order to run all test cases with single click.

--- app/src/androidTest/java/ercanduman/visualizerestaurant/UITestSuite.kt
+++ app/src/androidTest/java/ercanduman/visualizerestaurant/UITestSuite.kt
@@ -1,20 +1,26 @@
 package ercanduman.visualizerestaurant
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import ercanduman.visualizerestaurant.ui.main.MainActivityTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.junit.runners.Suite
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
+ * Test Suite combines all tests and gives ability to run all test cases with single click.
+ *
+ * When you run this class (UITestSuite.class), it will run all the tests in all the suite classes.
+ *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class
+)
+class UITestSuite {
     @Test
     fun useAppContext() {
         // Context of the app under test.
