ddf168f67f3bd13fdd6f8fbd0c79fbac268a23b7
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-12-06 16:41:58 +0300

    viewBinding implementation added to AppAdapter.kt

--- app/src/main/java/ercanduman/visualizerestaurant/ui/main/AppAdapter.kt
+++ app/src/main/java/ercanduman/visualizerestaurant/ui/main/AppAdapter.kt
@@ -1,16 +1,14 @@
 package ercanduman.visualizerestaurant.ui.main
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ercanduman.visualizerestaurant.R
 import ercanduman.visualizerestaurant.data.db.entity.Restaurant
+import ercanduman.visualizerestaurant.databinding.ListItemRestaurantBinding
 
 /**
  * Responsible to provide views that represents items in list.
@@ -70,28 +68,43 @@ class AppAdapter(private val listener: ItemClickListener) :
      */
     fun getCurrentItem(position: Int): Restaurant = listDiffer.currentList[position]
 
-    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val favorite: ImageView = itemView.findViewById(R.id.item_favorite)
-        val itemName: TextView = itemView.findViewById(R.id.item_name)
-        val openingState: TextView = itemView.findViewById(R.id.item_opening_state)
-        val sortValue: TextView = itemView.findViewById(R.id.item_sorting_value)
+    inner class ItemViewHolder(private val binding: ListItemRestaurantBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        /**
-         * Item clicks will be passed to listener
-         */
-        init {
-            favorite.setOnClickListener {
-                val position = adapterPosition
+        fun bind(restaurant: Restaurant) {
+            binding.apply {
+                val context = itemView.context
+                itemName.text = restaurant.name
+                val openingStateText =
+                    context.getString(R.string.format_opening_state, restaurant.status)
+                itemOpeningState.text = openingStateText
 
-                /**
-                 * Check if position is valid.
-                 *
-                 * In delete or new insertion processes if list item clicked,
-                 * it is possible to click on an item which is animating during process
-                 * and its position might be invalid.
-                 */
-                if (position != RecyclerView.NO_POSITION) {
-                    listener.onItemClicked(position)
+                val sortValueText =
+                    context.getString(R.string.format_sort_value, getSortValue(restaurant))
+                itemSortingValue.text = sortValueText
+
+                val drawable =
+                    if (restaurant.isFavorite) R.drawable.ic_favorite else R.drawable.ic_favorite_border
+                itemFavorite.apply {
+                    setImageDrawable(ContextCompat.getDrawable(context, drawable))
+
+                    /**
+                     * Item clicks will be passed to listener
+                     */
+                    setOnClickListener {
+                        val position = adapterPosition
+
+                        /**
+                         * Check if position is valid.
+                         *
+                         * In delete or new insertion processes if list item clicked,
+                         * it is possible to click on an item which is animating during process
+                         * and its position might be invalid.
+                         */
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClicked(position)
+                        }
+                    }
                 }
             }
         }
@@ -99,21 +112,7 @@ class AppAdapter(private val listener: ItemClickListener) :
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val restaurant = listDiffer.currentList[position]
-        holder.apply {
-            val context = itemView.context
-            itemName.text = restaurant.name
-            val openingStateText =
-                context.getString(R.string.format_opening_state, restaurant.status)
-            openingState.text = openingStateText
-
-            val sortValueText =
-                context.getString(R.string.format_sort_value, getSortValue(restaurant))
-            sortValue.text = sortValueText
-
-            val drawable =
-                if (restaurant.isFavorite) R.drawable.ic_favorite else R.drawable.ic_favorite_border
-            holder.favorite.setImageDrawable(ContextCompat.getDrawable(context, drawable))
-        }
+        holder.bind(restaurant)
     }
 
     private fun getSortValue(restaurant: Restaurant): String {
@@ -130,10 +129,9 @@ class AppAdapter(private val listener: ItemClickListener) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item_restaurant, parent, false)
-        )
+        val binding =
+            ListItemRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
 
     override fun getItemCount(): Int = listDiffer.currentList.size
